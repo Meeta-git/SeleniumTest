34fe7be2749d9aff2f2fedae03230bed2f2d363a
MeetaW <w.meeta@gmail.com>
2024-04-03 20:28:36 -0500

    Open gmail page.

--- src/com/msi/app/LaunchBrowser.java
+++ src/com/msi/app/LaunchBrowser.java
@@ -12,20 +12,19 @@ public class LaunchBrowser {
 		
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		
 		driver.get("https://www.gmail.com/");
-		//driver.findElement(By.className("RNNXgb")).sendKeys("gmail");
-		
+		driver.findElement(By.xpath("//div[@class='Xb9hP']/input[@type='email']")).sendKeys("w.meeta@gmail.com");
+		//driver.findElement(By.className("VfPpkd-RLmnJb")).click();
 	}
 	
 	
-	public void openSafari() {
+	/*public void openSafari() {
 		
 		WebDriver driver = new SafariDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.google.co.in/");
 		
-	}
+	}*/
 	
 	
 }
